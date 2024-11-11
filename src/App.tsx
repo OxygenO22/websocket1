548d69d1296b09052c2ac6626b5b5e9518cf3683
OxygenO22 <bamaik@mail.ru>
2024-11-11 15:53:56 +0300

    Reminding reduxTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Counter } from './Counter/Counter';
+import "./App.css";
+import { Counter } from "./features/counter/Counter";
+import logo from "./logo.svg";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Counter />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span> and </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/tutorials/essentials/part-2-app-structure"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
